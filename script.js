cbdca04723a2053066c78039661fc3937787ad45
aminahnawidz <132647517+aminahnawidz@users.noreply.github.com>
2023-05-08 02:12:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const menuicon = document.querySelector('.header .nav-bar .nav-list .menuicon');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+menuicon.addEventListener('click', () => {
+	menuicon.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		menuicon.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
